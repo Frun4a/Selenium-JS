bee7a72e70a3d14e994ae01d1359f91bb90d8b17
Frun4a <rivennike@gmail.com>
2019-05-26 20:22:29 -0700

    Added explicit waits for submitBtn

--- learn.js
+++ learn.js
@@ -13,19 +13,27 @@ async function main() {
   const driver = await new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
 
   await driver.get('https://library-app.firebaseapp.com/');
-  await driver.manage().setTimeouts( {implicit: 5000} );
+  //await driver.manage().setTimeouts( {implicit: 5000} );
+  let submitBtn = await driver.findElement(By.css('.btn-lg'));
+  await driver.findElement(By.css('input')).sendKeys('user');
 
-  await driver.findElement(By.css('input')).sendKeys('user@email.com');
+  await driver.wait(() => submitBtn.getCssValue('opacity').then((opacity) =>{
+    //console.log(opacity);
+    return  opacity === '1';
+  }), 10000);
+  
+  await submitBtn.click();
 
-  await driver.findElement(By.css('.btn-lg')).click();
-  await driver.sleep(1000);
+  //await driver.findElement(By.css('.btn-lg')).click();
 
-  await driver.findElement(By.css('.alert-success'))
+  await driver.wait(until.elementLocated(By.css('.alert-success')), 5000)
     .getText()
     .then(text => console.log(`Success message appeared: ${text}`));
 
-  await driver.findElements(By.css('nav li'))
-    .then(array => array.map(el => el.getText().then(text => text !='' ? console.log(`Text of the nav element is: ${text}`) : null)));
+  //await driver.findElements(By.css('nav li'))
+  //  .then(array => array.map(el => el.getText().then(text => text !='' ? console.log(`Text of the nav element is: ${text}`) : null)));
+
+
 
   await setTimeout(() => driver.quit(), 100);
 }
